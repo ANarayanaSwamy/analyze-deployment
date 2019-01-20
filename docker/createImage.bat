4eed50cf7d0adcdb08a16e1d1b8f89a83b84df96
NarayanaSwamy <narayanaswamy.a@gmail.com>
2019-01-20 20:53:21 +1100

    Update storage

--- docker/createImage.bat
+++ docker/createImage.bat
@@ -1,11 +1,13 @@
 @echo off
 echo Creating image
-set ROOT_PATH=D:/GaniWork/EclipseWorkspace/VideoAnalytics
+set CURR_FOLDER=%CD%
+set ROOT_PATH=%CURR_FOLDER%/../../
+echo Using root path %ROOT_PATH%
 REM cd %ROOT_PATH%/console
 REM call mvn clean install
 REM echo Making the docker image
-cd %ROOT_PATH%/deployment/docker
+cd %ROOT_PATH%/analyse-deployment/docker
 del console-1.0.0.jar
-cp %ROOT_PATH%\console\target\console-1.0.0.jar console-1.0.0.jar
+cp %ROOT_PATH%\analyze-console\target\console-1.0.0.jar console-1.0.0.jar
 call docker build -t console .
 call pushImage.bat
