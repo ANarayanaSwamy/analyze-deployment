d6429074fd29a2f91b76e9fdbd42117372c8235c
NarayanaSwamy <narayanaswamy.a@gmail.com>
2019-01-21 22:17:26 +1100

    Updated files

--- docker/createImage.bat
+++ docker/createImage.bat
@@ -6,7 +6,7 @@ echo Using root path %ROOT_PATH%
 REM cd %ROOT_PATH%/console
 REM call mvn clean install
 REM echo Making the docker image
-cd %ROOT_PATH%/analyse-deployment/docker
+cd %ROOT_PATH%/analyze-deployment/docker
 del console-1.0.0.jar
 cp %ROOT_PATH%\analyze-console\target\console-1.0.0.jar console-1.0.0.jar
 call docker build -t console .
--- kubernetes/clean.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-call kubectl delete deployments --all
-call kubectl delete services --all
-call kubectl delete pods --all
-call kubectl delete pv --all
-
-call minikube addons disable default-storageclass
-call kubectl apply -f minikube-storage.yaml
-REM minikube --ip 192.168.99.1 mount "C:\\Users\\Dell\\Desktop\\pod_data:/minikube-host"
